d8b44aa8cc30fdf2a53e6e1d8a34869e5b89be8b
Vincent Salazar <57049365+vincesalazar@users.noreply.github.com>
2020-05-22 23:52:30 -0700

    Add files via upload

--- cash_task_app/models.py
+++ cash_task_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from time_man_app.models import User
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    city = models.CharField(max_length=100, default="")
+    state = models.CharField(max_length = 2, default="")
+    user = models.ForeignKey(User, related_name="jobs", on_delete=models.CASCADE, default = "")
+    pinned = models.ManyToManyField(User, related_name="pins" , default = "")
+
+    def __repr__(self):
+        return f"title: {self.title}, city: {self.city}, user email: {self.user.email}"
 
 # Create your models here.
