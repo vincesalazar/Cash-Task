9c9844d2c60169645b55e7241dbfcfa43cd95531
Vincent Salazar <57049365+vincesalazar@users.noreply.github.com>
2020-05-23 23:31:25 -0700

    Add files via upload

--- time_man_app/admin.py
+++ time_man_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from time_man_app.models import User, Task, Collection
+from cash_task_app.models import Job
 
 admin.site.register(User)
 admin.site.register(Task)
 admin.site.register(Collection)
+admin.site.register(Job)
 # Register your models here.
