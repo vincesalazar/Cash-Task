7ce81e66fb2dc9bba39b32ea4f5c761af93851da
Vincent Salazar <57049365+vincesalazar@users.noreply.github.com>
2020-05-23 23:03:49 -0700

    Add files via upload

--- cash_task_app/urls.py
+++ cash_task_app/urls.py
@@ -5,13 +5,16 @@ urlpatterns = [
     # TEMPLATES
     path('', views.index),
     path('homepage', views.homepage),
+    path("createJob", views.createJobPage),
+    # PRACTICE PAGINATION
+    path("pagination", views.pagination),
     # LOGIN REG PROCESS
     path('register', views.register),
     path('login', views.login),
     path('logout', views.logout),
     # JOB PROCESS
-    path('createJob', views.createJob),
+    path('creatingJob', views.createJob),
     path('deleteJob/<int:id>', views.deleteJob),
-    # path('updateJob', views.updateJob),
-    # path('pinJob', views.pinJob),
+    path('updateJob/<int:id>', views.updateJob),
+    path('pinJob/<int:id>', views.pinJob),
 ]
